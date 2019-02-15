b95c5eff619b6e19d7f5a8f7467dffae63e1829a
dangtuan21 <dangtuan21@gmail.com>
2019-02-15 09:44:52 -0500

    comp fetch 2

--- servers/compliance/compliance-callback/server.js
+++ servers/compliance/compliance-callback/server.js
@@ -19,27 +19,6 @@ app.post('/compliance/fetch_info', function (request, response) {
     var addressParts = response.body.address.split('*');
     var friendlyId = addressParts[0];
     console.log('friendlyId', friendlyId);
-  
-    /*
-    // You need to create `accountDatabase.findByFriendlyId()`. It should look
-    // up a customer by their Stellar account and return account information.
-    accountDatabase.findByFriendlyId(friendlyId)
-      .then(function(account) {
-        // This can be any data you determine is useful and is not limited to
-        // these three fields.
-        response.json({
-          name: account.fullName,
-          address: account.address,
-          date_of_birth: account.dateOfBirth
-        });
-        response.end();
-      })
-      .catch(function(error) {
-        console.error('Fetch Info Error:', error);
-        response.status(500).end(error.message);
-      });
-
-      */
 
     var account = findCustomerByFriendlyId(friendlyId);
     response.json({
